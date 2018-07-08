e1533c949c6aa9b37011841dd44c2d057e02d2b1
Sihong Huang <sihongspring2011@gmail.com>
2018-07-08 13:24:50 -0400

    work on authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import {
   MatInputModule,
   MatCardModule,
@@ -17,21 +17,27 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
+import { LoginComponent } from './auth/login/login.component';
 
 import { PostsService } from './posts/posts.service';
 import { AppRoutingModule } from './app.routing.module';
+import { SignupComponent } from './auth/signup/signup.component';
+import { AuthInceptor } from './auth/auth-interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
     HeaderComponent,
-    PostListComponent
+    PostListComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    FormsModule,
     BrowserAnimationsModule,
     HttpClientModule,
     MatInputModule,
@@ -42,7 +48,7 @@ import { AppRoutingModule } from './app.routing.module';
     MatProgressSpinnerModule,
     MatPaginatorModule
   ],
-  providers: [PostsService],
+  providers: [PostsService, {provide: HTTP_INTERCEPTORS, useClass: AuthInceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
